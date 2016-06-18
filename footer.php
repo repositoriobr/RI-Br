13f33a246d494bf7398be0c4e2517a215bb378e9
repositoriobr <brenolessa12@hotmail.com>
2016-06-18 09:34:39 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer class="ls-footer" role="contentinfo">
   <div class="ls-footer-info">
-    <p class="ls-copy-center" style="text-decoration: none;">2015-<?= date('Y');?> <a href="#" class="ls-tooltip-right" aria-label="Repositório Institucional do IFBA - Vitória da Conquista">RIIFBA</a>. Desenvolvido por <a href="http://lattes.cnpq.br/5590242674758693" target="__blank" class="lattes" title="Lattes">Breno Lessa</a> e <a href="http://lattes.cnpq.br/7272567597428470" target="__blank" class="lattes" title="Lattes">Moara Brito</a>.</p>
+    <p class="ls-copy-center" style="text-decoration: none;">2015-<?= date('Y');?> <a href="#" class="ls-tooltip-right" aria-label="Repositório Institucional">RI-Br</a>. Desenvolvido por <a href="http://lattes.cnpq.br/5590242674758693" target="__blank" class="lattes" title="Lattes">Breno Lessa</a> e <a href="http://lattes.cnpq.br/7272567597428470" target="__blank" class="lattes" title="Lattes">Moara Brito</a>.</p>
   </div>
 </footer>
